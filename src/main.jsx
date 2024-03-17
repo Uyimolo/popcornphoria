07bb023b44e32d08ac0b17e1b6908fae4071399d
Urhoghide Justin Uyioghosa <uyijustindavid@gmail.com>
2024-03-17 17:22:26 +0100

    set up redux boiler plate and set up data fetching with redux toolkit query.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import store from './app/store.js';
+import { Provider } from 'react-redux';
+// import { ApiProvider } from '@reduxjs/toolkit/query/react';
+// import apiSlice from './features/apiSlice.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
