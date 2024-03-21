4c2adfd8aef8b89bcdcc0f859df5e3785fe2a2bf
Urhoghide Justin Uyioghosa <uyijustindavid@gmail.com>
2024-03-21 06:42:32 +0100

    fixed tv details page trailer video not covering up to 100% width

--- src/components/MovieListPagination.jsx
+++ src/components/MovieListPagination.jsx
@@ -1,15 +1,14 @@
-
-const MovieListPagination = ({movieList}) => {
+const MovieListPagination = ({ movieList }) => {
   return (
     <div>
-      {movieList.length > 0 && (
+      {/* {movieList.length > 0 && (
         <div className='pagination'>
           <button className='prev' onClick={() => movePage('prev')}>Prev</button>
           <button className='next' onClick={() => movePage('next')}>Next</button>
         </div>
-      )}
+      )} */}
     </div>
-  )
-}
+  );
+};
 
-export default MovieListPagination
+export default MovieListPagination;
