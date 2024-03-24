847401f085cd83442298b24a231372d18f409c7f
Urhoghide Justin Uyioghosa <uyijustindavid@gmail.com>
2024-03-24 05:38:27 +0100

    fixed image not loading

--- src/components/LazyCarouselImage.jsx
+++ src/components/LazyCarouselImage.jsx
@@ -1,18 +1,27 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const LazyCarouselImage = ({ poster_path, title }) => {
   const [isLoading, setIsLoading] = useState(true);
 
   const handleImageLoad = () => {
-    setIsLoading(false);
+    // setIsLoading(prevState => !prevState);
+    alert(isLoading)
   };
+
+  useEffect(() => {
+    const image = new Image();
+    image.onload = () => {
+      setIsLoading(false);
+    };
+    image.src = `https://image.tmdb.org/t/p/w200${poster_path}`;
+  }, [poster_path]);
   return (
     <div>
       {
         <img
-          onLoad={handleImageLoad}
+          // onLoad={() => setIsLoading(false)}
           loading='lazy'
-          src={`https://image.tmdb.org/t/p/w92${poster_path}`}
+          src={`https://image.tmdb.org/t/p/w200${poster_path}`}
           alt={`${title}`}
           style={{ display: isLoading ? 'none' : 'block' }}
         />
