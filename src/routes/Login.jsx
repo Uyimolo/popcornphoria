069d8c3ac4a2798d3382820ae023ec402beed34e
Urhoghide Justin Uyioghosa <uyijustindavid@gmail.com>
2024-03-28 21:05:05 +0100

    created auth pages as users can't add to their watchlist on multiple devices  without been authenticated else watchlist will be saved to local storage and unavailable on other devices, added email and password auth and also google auth

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,17 +1,72 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { auth, googleProvider } from '../firebase/config';
+import { signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
+
 const Login = () => {
+  const [loginData, setLoginData] = useState({ email: '', password: '' });
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setLoginData({ ...loginData, [name]: value });
+  };
+
+  const handleSignin = async (e) => {
+    e.preventDefault();
+    const { email, password } = loginData;
+    try {
+      const isSignedin = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      if (isSignedin) {
+        console.log('signed in');
+        navigate('/');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleGoogleSignin = async (e) => {
+    e.preventDefault();
+    try {
+      const isGoogle = signInWithPopup(auth, googleProvider);
+      if (isGoogle) navigate('/');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className='page login-page'>
       <form action='' className='auth'>
-        <h3>Nice to see you again.</h3>
-        <input type='text' placeholder='Enter email' />
-        <input type='text' placeholder='Enter password' />
+        <h3>It's nice to see you again.</h3>
+        <input
+          type='text'
+          placeholder='Enter email'
+          name='email'
+          value={loginData.email}
+          onChange={handleChange}
+        />
+        <input
+          type='text'
+          placeholder='Enter password'
+          name='password'
+          value={loginData.password}
+          onChange={handleChange}
+        />
         <p className='forgot-password'>Forgot password?</p>
-        <button>Sign in</button>
+        <button onClick={handleSignin}>Sign in</button>
         <p className='or'>Or</p>
-        <button>Sign in with Google</button>
+        <button onClick={handleGoogleSignin}>Sign in with Google</button>
         <p className='no-account'>
-          Don't have an account? <Link className="link" to="/signup">Sign up now</Link>
+          Don't have an account?{' '}
+          <Link className='link' to='/signup'>
+            Sign up now
+          </Link>
         </p>
       </form>
     </div>
--- src/routes/Signup.jsx
+++ src/routes/Signup.jsx
@@ -1,18 +1,74 @@
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { auth, googleProvider } from '../firebase/config';
+import { createUserWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 
 const Signup = () => {
+  const [signupData, setSignupData] = useState({ email: '', password: '' });
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setSignupData({ ...signupData, [name]: value });
+  };
+
+  const handleEmailSignup = async (e) => {
+    e.preventDefault();
+    const { email, password } = signupData;
+    try {
+      const isSignedUp = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      if (isSignedUp) {
+        // create logic to return user to initial page where he was before with redux
+        navigate('/');
+      }
+      // setup elaborate error messages and toastify like alerts and input specific validations
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleGoogleSignup = async (e) => {
+    e.preventDefault();
+    try {
+     const isGoogle = await signInWithPopup(auth, googleProvider);
+     if(isGoogle) navigate('/')
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className='page signup-page'>
       <form action='' className='auth'>
         <h3>It's nice to see you.</h3>
-        <input type='text' placeholder='Enter email' />
-        <input type='text' placeholder='Enter password' />
-        <p className='forgot-password'>Forgot password?</p>
-        <button>Sign in</button>
+        <input
+          type='text'
+          placeholder='Enter email'
+          name='email'
+          value={signupData.email}
+          onChange={handleChange}
+        />
+        <input
+          type='text'
+          placeholder='Enter password'
+          name='password'
+          value={signupData.password}
+          onChange={handleChange}
+        />
+        <p className='secure-password'>Use a secure password!</p>
+        <button onClick={handleEmailSignup} className='signup-button'>
+          Sign up
+        </button>
         <p className='or'>Or</p>
-        <button>Sign in with Google</button>
+        <button onClick={handleGoogleSignup}>Sign up with Google</button>
         <p className='no-account'>
-          Already have an account? <Link to="/login">Sign in now</Link>
+          Already have an account?{' '}
+          <Link to='/login' className='link'>
+            Sign in now
+          </Link>
         </p>
       </form>
     </div>
